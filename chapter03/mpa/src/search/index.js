71f6591f93c9189473115ed62cb72b2745d7bd73
JaimeCheng <403693553@qq.com>
2019-10-28 11:23:04 +0800

    plugin-syntax-dynamic-import

--- chapter03/mpa/src/search/index.js
+++ chapter03/mpa/src/search/index.js
@@ -11,12 +11,29 @@ import '../static/search.less'
 import '../static/style.css'
 
 class Search extends React.Component {
+
+  constructor () {
+    super(...arguments)
+    this.state = {
+      Text: null
+    }
+  }
+
+  loadComponent () {
+    import('./text.js').then(Text => {
+      this.setState({
+        Text: Text.default
+      })
+    })
+  }
   render () {
     const funcA = a()
+    const { Text } = this.state
     return <div className="search-text">
       { funcA } Seacch Demo
       <br />
-      <img className="search-logo" src={ logo } />
+      { Text ? <Text /> : null }
+      <img className="search-logo" src={ logo } onClick={ this.loadComponent.bind(this) } />
       <img className="search-head" src={ avatar } />
     </div>
   }
