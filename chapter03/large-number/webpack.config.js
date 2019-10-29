b407343d24d2b3a9f91bd2b3ad7d1d68cbc77153
JaimeCheng <403693553@qq.com>
2019-10-29 10:07:32 +0800

    export large-number

--- chapter03/large-number/webpack.config.js
+++ chapter03/large-number/webpack.config.js
@@ -1,3 +1,22 @@
+const TerserPlugin = require('terser-webpack-plugin')
 module.exports = {
-  mode: 'none'
+  mode: 'none',
+  entry: {
+    'large-number': './src/index.js',
+    'large-number.min': './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    library: 'largeNumber',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        include: /\.min\.js$/
+      })
+    ]
+  }
 }
