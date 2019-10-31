4f64d239828d9d8a770eb2f50bcd8dc3e30fc7a8
JaimeCheng <403693553@qq.com>
2019-10-31 13:36:41 +0800

    sync loader

--- chapter07/raw-loader/src/raw-loader.js
+++ chapter07/raw-loader/src/raw-loader.js
@@ -1,7 +1,17 @@
+const loaderUtils = require('loader-utils');
+
 module.exports = function (source) {
+  const  { name } = loaderUtils.getOptions(this);
+  console.log('name:', name);
+
   const json = JSON.stringify(source)
     .replace('foo', 'jaime-')
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
-  return `export default ${ json }`;
+
+  // throw new Error('Error'); // sync loader 异常处理 方法1
+  // this.callback(new Error('Error'), json); // sync loader 异常处理 方法2
+  
+  // return `export default ${ json }`; // sync loader 返回值 方式1
+  this.callback(null, json, 1, 3); // sync loader 返回值 方式2 可返回多个值
 }
