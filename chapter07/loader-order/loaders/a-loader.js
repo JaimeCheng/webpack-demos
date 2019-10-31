8e92a1f16484617b5d46ac63815aef827efe32fd
JaimeCheng <403693553@qq.com>
2019-10-31 14:47:36 +0800

    emitFile

--- chapter07/loader-order/loaders/a-loader.js
+++ chapter07/loader-order/loaders/a-loader.js
@@ -1,4 +1,10 @@
+const loaderUtils = require('loader-utils');
+
 module.exports = function (source) {
   console.log('Loader A is excuted!');
+  const url = loaderUtils.interpolateName(this, '[name].txt', source);
+
+  console.log(url);
+  this.emitFile(url, source);
   return source;
 }
